ab9c64e41110e22b07e3b7a4b5b3dd4eb056ceb2
Saloni Darji <salonidarji3335@gmail.com>
2023-06-11 19:43:33 +0530

    daily and hourly views completed.

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 
-import { TextField, Typography } from '@mui/material';
+import { Dialog, TextField, Typography , DialogTitle, DialogActions, DialogContent} from '@mui/material';
 import Button from '@mui/material/Button/Button';
 import Grid from '@mui/material/Grid/Grid';
 import { signOut } from 'next-auth/react';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid, GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
+import axios from 'axios';
 
 export default function List() {
     const [searchText, setSearchText] = useState("");
     const [data, setData] = useState([]);
     const [rowSelectionModel, setRowSelectionModel] = useState<GridRowSelectionModel>([]);
     const [siteInfo, setSiteInfo] = useState("")
-
+    const [searchInMainOneDay, setSearchInMainOneDay] = useState("");
+    const [searchInOneDay, setSearchInOneDay] = useState("");
+    const [searchInOneHour, setSearchInOneHour] = useState("");
+    const [showDialog, setShowDialog] = useState(false)
+    const [pageDetailURL,setPageDetailURL] = useState("");
     console.log("date:", Date.now())
     
     const fetchData = async (text:string) => {
         const urlForListSearch = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=${text}&meta=siteviews&svlimit=24&utf8=1&svprop=views`
-        const req = await fetch(urlForListSearch);
-        const res = await req?.json();
+        let req = await fetch(urlForListSearch);
+        let res = await req?.json();
         console.log("res", res)
-        const searchData = await res?.query?.search;
+        let searchData = await res?.query?.search;
         console.log("searchData:", searchData)
         setSiteInfo(res?.query?.siteviews || {})
         return setData(searchData || []);
@@ -36,9 +41,10 @@ export default function List() {
               )
               .then(
                 function (response) {
-                      console.log("detail response:", response.query.pages);
-                      const pageDetailURL = response.query.pages[pageID].fullurl || "";
-                      window.open(pageDetailURL, "_blank")
+                      console.log("detail response:", response?.query?.pages);
+                      setPageDetailURL(response.query.pages[pageID].fullurl || "");
+                    //   window.open(pageDetailURL, "_blank")
+                      getDailySiteViews(pageID);
                 }
               )
     }
@@ -58,11 +64,42 @@ export default function List() {
         { field: 'title', headerName: 'Title', width:400 },
     ];
 
-    let searchInOneDay = "";
-    Object.keys(siteInfo)?.map((key: any, index, arr) => {
-        if (index === arr.length - 1) {
-            searchInOneDay = siteInfo[key];
-    }  });
+  
+    useEffect(() => {
+        Object.keys(siteInfo)?.map((key: any, index, arr) => {
+            if (index === arr.length - 1) {
+                setSearchInMainOneDay(siteInfo[key]);
+            }
+        });
+    },[siteInfo])
+    
+    
+    const handleClose = () => {
+        setShowDialog(false)
+        setSearchInOneDay("");
+        setSearchInOneHour("");
+        setPageDetailURL("")
+   }
+
+    async function getDailySiteViews(pageId: string) {
+        const filteredData  = data.find((obj: {pageid:string}) => obj.pageid === pageId) || {title:""};
+        console.log("filterd data: ", filteredData)
+        const url = `https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/${filteredData.title}/daily/20230610/20230611`;
+      
+       
+        try {
+          const response = await axios.get(url);
+          const items = response.data.items;
+      
+          // Process the data as needed
+            console.log(items); // Array of objects containing hourly views data
+            const views = items[0].views;
+            setSearchInOneDay(views)
+            setSearchInOneHour(Math.floor(views/24).toString())
+        } catch (error) {
+          console.error('Error occurred while fetching site views:', error);
+        }
+      }
 
     return (
         <Grid container spacing={2}>
@@ -74,7 +111,7 @@ export default function List() {
             </Grid>
 
             <Grid item xs={12} style={{ height: 400, width: '100%' }}>
-            {searchInOneDay && (<Typography component={'h6'}>{`Total Search performed in one day: ${searchInOneDay}`}</Typography>)}
+            {searchInMainOneDay && (<Typography component={'h6'}>{`Total Search performed on main wikipedia in one day: ${searchInMainOneDay}`}</Typography>)}
                 {data?.length > 0 && (
                     <><DataGrid
                         rows={data}
@@ -89,10 +126,20 @@ export default function List() {
                         onRowSelectionModelChange={(newRowSelectionModel) => {
                             setRowSelectionModel(newRowSelectionModel);
                             fetchPageDetail(newRowSelectionModel[0]);
+                            setShowDialog(true)
                             console.log("row:", newRowSelectionModel);
                         } }
                         rowSelectionModel={rowSelectionModel} /></>)}
             </Grid>
+
+            <Dialog onClose={handleClose} open={showDialog}>
+                <DialogTitle>Page Details</DialogTitle>
+                <DialogContent>
+                    <Typography component={'h6'}>Search performed in one day: {searchInOneDay}</Typography>
+                    <Typography component={'h6'}>Search performed per Hour: {searchInOneHour}</Typography>
+                    <a href={pageDetailURL} style={{color:"blue"}} target='_blank'>show details page</a>
+                </DialogContent>
+            </Dialog>
         </Grid>
        
         
